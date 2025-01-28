86ef13f34c7af654e18473c7b2d8973422d7a2b0
Minji6 <105301353+Minji6@users.noreply.github.com>
2025-01-29 01:16:50 +0900

    Update ArchiveService.java

--- sequence_member/src/main/java/sequence/sequence_member/archive/service/ArchiveService.java
+++ sequence_member/src/main/java/sequence/sequence_member/archive/service/ArchiveService.java
@@ -1,23 +1,40 @@
 package sequence.sequence_member.archive.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sequence.sequence_member.archive.dto.ArchiveDTO;
 import sequence.sequence_member.archive.entity.ArchiveEntity;
 import sequence.sequence_member.archive.repository.ArchiveRepository;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class ArchiveService {
     private final ArchiveRepository archiveRepository;
 
-    //전체 아카이브 조회
-    
+    // Create 생성
+    @Transactional
+    public ArchiveDTO saveArchive(ArchiveDTO archiveDTO) {
+        ArchiveEntity archiveEntity = ArchiveEntity.builder()
+                .title(archiveDTO.getTitle())
+                .description(archiveDTO.getDescription())
+                .duration(archiveDTO.getDuration())
+                .field(archiveDTO.getField())
+                .status(archiveDTO.getStatus()) // byte 값 그대로 저장
+                .build();
 
+        ArchiveEntity saveArchive = archiveRepository.save(archiveEntity);
+
+        return ArchiveDTO.builder()
+                .archiveId(saveArchive.getArchiveId())
+                .title(saveArchive.getTitle())
+                .description(saveArchive.getDescription())
+                .duration(saveArchive.getDuration())
+                .field(saveArchive.getField())
+                .status(saveArchive.getStatus())
+                .build();
+    }
+    
+    // Read 조회
+    
 }
