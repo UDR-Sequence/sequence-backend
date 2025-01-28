279b98386d4569c28aae6ea7179d0daf5e0f57ec
Minji6 <105301353+Minji6@users.noreply.github.com>
2025-01-28 23:59:03 +0900

    feat : 아카이브 엔티티 생성

---
+++ sequence_member/src/main/java/sequence/sequence_member/archive/entity/ArchiveEntity.java
@@ -0,0 +1,45 @@
+package sequence.sequence_member.archive.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+import org.hibernate.type.descriptor.jdbc.TinyIntJdbcType;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Archive")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ArchiveEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "archiveId", nullable = false)
+    private Long archiveId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "duration", nullable = false)
+    private String duration;
+
+    @Column(name = "field", nullable = false)
+    private String field;
+
+    @Column(name = "status", nullable = false)
+    private TinyIntJdbcType status;
+
+    @ManyToMany
+    @JoinTable(
+            name = "ArchiveMember",
+            joinColumns = @JoinColumn(name = "archiveId"),
+            inverseJoinColumns = @JoinColumn(name = "memberId")
+    )
+    private List<ArchiveMemberEntity> archiveMembers;
+}
