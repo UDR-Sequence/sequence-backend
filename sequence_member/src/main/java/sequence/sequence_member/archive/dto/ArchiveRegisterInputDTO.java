e39a9b2e39ed4928015fcca13a4910812dbd4658
Minji6 <105301353+Minji6@users.noreply.github.com>
2025-02-08 03:51:08 +0900

    Create ArchiveRegisterInputDTO.java

---
+++ sequence_member/src/main/java/sequence/sequence_member/archive/dto/ArchiveRegisterInputDTO.java
@@ -0,0 +1,50 @@
+package sequence.sequence_member.archive.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
+import sequence.sequence_member.global.enums.enums.Category;
+import sequence.sequence_member.global.enums.enums.Period;
+import sequence.sequence_member.global.enums.enums.Status;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArchiveRegisterInputDTO {
+
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @Length(min = 1, max = 40, message = "제목은 40자 이하로 입력해주세요.")
+    private String title;
+
+    @NotEmpty(message = "설명을 입력해주세요.")
+    @Length(min = 1, max = 450, message = "설명은 450자 이하로 입력해주세요.")
+    private String description;
+
+    @NotEmpty(message = "기간을 입력해주세요.")
+    private String duration;
+
+    @NotNull(message = "카테고리를 선택해주세요.")
+    private Category category;
+
+    @NotNull(message = "기간을 선택해주세요.")
+    private Period period;
+
+    @NotNull(message = "상태를 선택해주세요.")
+    private Status status;
+
+    private String thumbnail;
+    private String link;
+
+    @NotEmpty(message = "관련 기술을 선택해주세요.")
+    @Size(max = 20, message = "관련 기술은 20개 이하로 선택해주세요.")
+    private List<String> skills;
+}
+
