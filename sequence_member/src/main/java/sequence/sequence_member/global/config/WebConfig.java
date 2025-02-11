7f1c7431e02365b975c560a7e88f352f5c26bd8c
Your Name <kim946509@gmail.com>
2025-02-12 00:02:42 +0900

    fix : springboot cors 문제 해결

---
+++ sequence_member/src/main/java/sequence/sequence_member/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package sequence.sequence_member.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type", "Access")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(false);
+    }
+}
