10cae9547d32a56b324fb656ba25e9c24d6faf39
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-03-08 00:16:45 +0900

    fix : 미니오 서버에 이미지 저장
    목적 :
    - 미니오 서버에 프로필 이미지를 저장
    - 저장된 프로필 이미지 url을 DB에 저장
    
    기타 :
    - 프로젝트 이미지 여러장 업로드 기능 구현 필요
    - 아카이브 썸네일 이미지 업로드 기능 구현 필요

--- sequence_member/src/main/java/sequence/sequence_member/global/minio/service/MinioService.java
+++ sequence_member/src/main/java/sequence/sequence_member/global/minio/service/MinioService.java
@@ -1,18 +1,18 @@
 package sequence.sequence_member.global.minio.service;
 
 
-import io.minio.GetObjectArgs;
-import io.minio.GetPresignedObjectUrlArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.RemoveObjectArgs;
+import io.minio.*;
 import io.minio.http.Method;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import sequence.sequence_member.global.annotation.MethodDescription;
 import sequence.sequence_member.global.exception.MinioException;
+import sequence.sequence_member.global.utils.FileExtension;
 import sequence.sequence_member.global.utils.MultipartUtil;
 
 
@@ -28,13 +29,42 @@ import sequence.sequence_member.global.utils.MultipartUtil;
 @Slf4j
 @RequiredArgsConstructor
 public class MinioService {
-    private final MinioClient minioClient;
-    private final MultipartUtil multipartUtil;
+
+    private final FileExtension fileExtension;
+
+    private MinioClient minioClient;
+
+    @Value("${MINIO_ENDPOINT}")
+    private String MINIO_ENDPOINT;
+
+    @Value("${MINIO_ACCESS_KEY}")
+    private String MINIO_ACCESS_KEY;
+
+    @Value("${MINIO_SECRET_KEY}")
+    private String MINIO_SECRET_KEY;
+
 
     @MethodDescription(description = "minio 서버에 파일 업로드")
     public String uploadFileMinio(String bucketName, String fileName, MultipartFile file) throws Exception {
+
         // 업로드 진행 전 파일 확장자 확인
         uploadFileCheck(file);
+
+        //미니오 서버와 연결
+        minioClient = MinioClient.builder()
+                .endpoint(MINIO_ENDPOINT)
+                .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
+                .build();
+
+
+        //해당 버킷이 존재하지 않는 경우 버킷을 새로 생성
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if(!found){
+            minioClient.makeBucket(
+                    MakeBucketArgs.builder()
+                            .bucket(bucketName)
+                            .build());
+        }
 
         minioClient.putObject(PutObjectArgs.builder()
                 .bucket(bucketName)
@@ -43,6 +73,7 @@ public class MinioService {
                 .contentType(file.getContentType())
                 .build());
 
+        //해당 이미지 url 리턴
         return minioClient.getPresignedObjectUrl(
                 GetPresignedObjectUrlArgs.builder()
                         .method(Method.GET)
@@ -50,12 +81,13 @@ public class MinioService {
                         .object(fileName)
                         .expiry(7, TimeUnit.DAYS)
                         .build());
+
     }
 
     @MethodDescription(description = "파일의 확장자를 확인합니다.")
     public void uploadFileCheck(MultipartFile file) {
-        String fileExtension = multipartUtil.getFileExtension(file);
-        multipartUtil.uploadFileExtensionCheck(fileExtension);
+        String extension = fileExtension.getFileExtension(file);
+        fileExtension.uploadFileExtensionCheck(extension);
     }
 
 
---
+++ sequence_member/src/main/java/sequence/sequence_member/global/utils/FileExtension.java
@@ -0,0 +1,36 @@
+package sequence.sequence_member.global.utils;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import sequence.sequence_member.global.annotation.MethodDescription;
+import sequence.sequence_member.global.exception.MultipartException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class FileExtension {
+
+    private String[] extensions = {"png", "jpeg", "jpg", "bmp"};
+    private List<String> allowedFileExtensions = new ArrayList<>(Arrays.asList(extensions));
+
+    @MethodDescription(description = "파일 확장자 추출")
+    public String getFileExtension(MultipartFile file) {
+        String originalFileName = file.getOriginalFilename();
+        if (StringUtils.hasText(originalFileName)) {
+            return originalFileName.substring(originalFileName.lastIndexOf('.') + 1);
+        }
+        return "";
+    }
+
+    @MethodDescription(description = "파일 확장자를 확인합니다.")
+    public void uploadFileExtensionCheck(String extension) {
+        if (!allowedFileExtensions.contains(extension)) {
+
+        }
+    }
+}
