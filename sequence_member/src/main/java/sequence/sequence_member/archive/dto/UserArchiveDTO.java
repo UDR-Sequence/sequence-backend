9546438ea7d7037c9c1c4f8fe68cc79bc431317e
Your Name <kim946509@gmail.com>
2025-03-12 21:16:07 +0900

    fix : UserArchiveDTO 생성자 수정

--- sequence_member/src/main/java/sequence/sequence_member/archive/dto/UserArchiveDTO.java
+++ sequence_member/src/main/java/sequence/sequence_member/archive/dto/UserArchiveDTO.java
@@ -1,17 +1,19 @@
 package sequence.sequence_member.archive.dto;
 
-import java.util.Date;
+import java.sql.Date;
 import lombok.Getter;
 import sequence.sequence_member.archive.entity.Archive;
 
 @Getter
 public class UserArchiveDTO {
 
-    private Long projectId;
+    private Long archiveId;
     private String archiveTitle;
     private Date createDate;
 
     public UserArchiveDTO(Archive archive){
-
+        this.archiveId = archive.getId();
+        this.archiveTitle = archive.getTitle();
+        this.createDate = Date.valueOf(archive.getCreatedDateTime().toLocalDate());
     }
 }
