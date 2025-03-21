17e8763c9497bcc920ad4912f51272ee20118736
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-21 20:56:36 +0900

    fix : 일단 url이 결정 안됐으므로 모두 허용

--- sequence_member/src/main/java/sequence/sequence_member/member/config/CorsMvcConfig.java
+++ sequence_member/src/main/java/sequence/sequence_member/member/config/CorsMvcConfig.java
@@ -9,6 +9,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowCredentials(true);
     }
 }
