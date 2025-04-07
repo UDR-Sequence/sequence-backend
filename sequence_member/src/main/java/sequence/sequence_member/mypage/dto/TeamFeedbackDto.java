3976d5a3c1a9403ab5f5f4ecf31a2dd5b209d45a
vkflco08 <vkflco08@naver.com>
2025-04-07 19:09:16 +0900

    [feat]: 마이페이지 response DTO 구조 수정
    [목적]: 요구사항 반영하여 반환 객체 수정을 위함
    
    [목표]:
      - 포트폴리오 객테에 참여하는 프로젝트 리스트 추가
      - 팀원 평가 객체 추가
    
    [달성도]:
      - 각 정보를 객체로 구분하여  구조 수정 완료.

---
+++ sequence_member/src/main/java/sequence/sequence_member/mypage/dto/TeamFeedbackDto.java
@@ -0,0 +1,15 @@
+package sequence.sequence_member.mypage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sequence.sequence_member.archive.dto.MyPageEvaluationDTO;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamFeedbackDto {
+    MyPageEvaluationDTO myPageEvaluation;
+}
