f02df702eeac1e3098ade4f64665b35a760b4e10
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-27 15:35:57 +0900

    fix : 맵형태로 enum과 string 모두 전달

--- sequence_member/src/main/java/sequence/sequence_member/member/controller/SkillController.java
+++ sequence_member/src/main/java/sequence/sequence_member/member/controller/SkillController.java
@@ -1,11 +1,13 @@
 package sequence.sequence_member.member.controller;
 
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sequence.sequence_member.global.enums.enums.Skill;
 import sequence.sequence_member.global.response.ApiResponseData;
 import sequence.sequence_member.member.service.SkillService;
 
@@ -18,8 +20,8 @@ public class SkillController {
 
     // 모든 스킬들을 조회하는 컨트롤러
     @GetMapping
-    public ResponseEntity<ApiResponseData<List<String>>> getAllSkills() {
-        List<String> skills = skillService.getAllSkills();
+    public ResponseEntity<ApiResponseData<Map<Skill,String>>> getAllSkills() {
+        Map<Skill, String> skills = skillService.getAllSkills();
         return ResponseEntity.ok(ApiResponseData.success(skills));
     }
 }
--- sequence_member/src/main/java/sequence/sequence_member/member/service/SkillService.java
+++ sequence_member/src/main/java/sequence/sequence_member/member/service/SkillService.java
@@ -2,6 +2,8 @@ package sequence.sequence_member.member.service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sequence.sequence_member.global.enums.enums.Skill;
@@ -10,9 +12,8 @@ import sequence.sequence_member.global.enums.enums.Skill;
 @RequiredArgsConstructor
 public class SkillService {
 
-    public List<String> getAllSkills(){
-        return Arrays.stream(
-                Skill.values()).map(Skill::getName)
-                .toList();
+    public Map<Skill,String> getAllSkills(){
+        return Arrays.stream(Skill.values())
+                .collect(Collectors.toMap(skill -> skill, Skill::getName));
     }
 }
