f876300b1f329761da7432238e3847d4acba6dde
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-05-26 03:17:24 +0900

    feat : 스킬 카테고리 전체 반환 기능 추가
    - 스킬 카테고리 전체 반환 기능 추가
    - 스킬 카테고리 List<String>으로 반환

--- sequence_member/src/main/java/sequence/sequence_member/member/service/SkillService.java
+++ sequence_member/src/main/java/sequence/sequence_member/member/service/SkillService.java
@@ -1,5 +1,6 @@
 package sequence.sequence_member.member.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,8 +13,15 @@ import sequence.sequence_member.global.enums.enums.Skill;
 @RequiredArgsConstructor
 public class SkillService {
 
-    public Map<Skill,String> getAllSkills(){
-        return Arrays.stream(Skill.values())
-                .collect(Collectors.toMap(skill -> skill, Skill::getName));
+
+    // 모든 스킬 카테고리들을 반환
+    public List<String> getAllSkills(){
+        List<String> skillNames = new ArrayList<>();
+
+        for(Skill skill : Skill.values()){
+            skillNames.add(skill.getName());
+        }
+
+        return skillNames;
     }
 }
