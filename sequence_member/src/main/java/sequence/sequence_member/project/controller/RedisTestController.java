42494b26f5067bfd3f0db041b72e75c1f4b2f8b1
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-06-21 02:54:23 +0900

    feat : 댓글, 레디스 컨트롤러 로깅
    - 댓글 controller api 요청 로깅
    - 레디스 테스트 controller api 요청 요깅

--- sequence_member/src/main/java/sequence/sequence_member/project/controller/RedisTestController.java
+++ sequence_member/src/main/java/sequence/sequence_member/project/controller/RedisTestController.java
@@ -1,11 +1,13 @@
 package sequence.sequence_member.project.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sequence.sequence_member.project.service.RedisTestService;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
@@ -14,7 +16,10 @@ public class RedisTestController {
 
     @GetMapping("/redis")
     public String testRedis() {
+        log.info("레디스 테스트 요청 : /test/redis GET request 발생");
+
         redisTestService.testRedisConnection();
+
         return "Redis 테스트 완료!";
     }
 }
