2915bd71d2b1836f25976c1c8c4854f808806b60
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-06-21 02:36:37 +0900

    feat : project service  에 에러 로깅 추가
    - project service 에러 로깅 추가

--- sequence_member/src/main/java/sequence/sequence_member/global/exception/AuthException.java
+++ sequence_member/src/main/java/sequence/sequence_member/global/exception/AuthException.java
@@ -1,9 +1,12 @@
 package sequence.sequence_member.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import sequence.sequence_member.global.response.Code;
 
+@Slf4j
 public class AuthException extends BaseException {
     public AuthException(String message) {
         super(Code.ACCESS_DENIED,message);
+        log.error("접근 불가");
     }
 }
